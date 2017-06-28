175a27c0077528b280c2a0b91befcdc684f2e73f
sujithamahendran <sujirenu2@gmail.com>
2017-06-28 10:11:40 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,a[100],i;
+clrscr();
+scanf("%d",&n);
+for(i=0;i<n;i++);
+{
+scanf("%d",&a[i]);
+if(i==a[i])
+printf("%d",i);
+}
+getch();
+}
